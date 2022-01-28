6eb6480c889ae4faea2cbef0ca3949260663dc5c
Ritvik <ritvikthkr16@gmail.com>
2022-01-28 13:17:17 +0530

    Seraching_folder

---
+++ Searching/facesun.java
@@ -0,0 +1,15 @@
+class Solution {
+    int countBuildings(int h[], int n) {
+        // code here
+        int max = h[0];
+        int count = 1;
+        int i = 0;
+        for(int j=i+1;j<n;j++){
+            if(h[j] > max){
+                max = h[j];
+                count = count+1;
+            }
+        }
+        return count;
+    }
+}
--- Strings/stringmat.java
+++ Strings/stringmat.java
@@ -4,6 +4,28 @@ class Main{
     public static String matchSub(String input, String comp){
         int N1 = input.length();
         int N2 = comp.length();
+        int i = 0;
+        int begin = 0;
+        int end = 0;
+        int count = 0;
+        for(int low=0,high=0;high<N1;high++){
+            if(input.charAt(high) == comp.charAt(i)){
+                high = high+1;
+                i = i+1;
+                count = count+1;
+                if(count == N2){
+                    begin = low;
+                    end = high;
+                    i = 0;
+                    while(count >= N2){
+                        low = low+1;
+                    }
+                }
+            }
+            else{
+                high = high+1;
+            }
+        }
     }
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
